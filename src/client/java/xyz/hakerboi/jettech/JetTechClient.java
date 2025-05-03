0adac3689ab5735ca43e8f52c68e9db133c7f2a8
Hakerboi <racerburhan@gmail.com>
2025-05-03 19:55:54 +0100

    Initial commit

---
+++ src/client/java/xyz/hakerboi/jettech/JetTechClient.java
@@ -0,0 +1,10 @@
+package xyz.hakerboi.jettech;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JetTechClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/xyz/hakerboi/jettech/JetTechDataGenerator.java
@@ -0,0 +1,11 @@
+package xyz.hakerboi.jettech;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class JetTechDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
